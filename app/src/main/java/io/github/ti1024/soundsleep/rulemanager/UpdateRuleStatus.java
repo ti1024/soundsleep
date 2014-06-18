f86285543d3f6b2ffc6c1c15b4742d3ebec74af7
Tsuyoshi Ito <tsuyoshi.ito.2006@gmail.com>
2014-06-18 23:58:55 +0900

    Add logging to rulemanager.UpdateRuleStatus

--- app/src/main/java/io/github/ti1024/soundsleep/rulemanager/UpdateRuleStatus.java
+++ app/src/main/java/io/github/ti1024/soundsleep/rulemanager/UpdateRuleStatus.java
@@ -3,11 +3,17 @@ package io.github.ti1024.soundsleep.rulemanager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class UpdateRuleStatus extends BroadcastReceiver {
+
+    private static String TAG = "rulemanager.UpdateRuleStatus";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast " + intent.toString());
         Rule rule = Rule.Load(context);
         RuleManager.updateRuleStatus(context, rule);
     }
+
 }
